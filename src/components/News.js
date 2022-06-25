b34caa160e7367513827d4ab2f747d73c6b42aed
Akhilesh <akhileshb318@gmail.com>
2022-06-25 15:25:55 +0530

    error fixing 4

--- src/components/News.js
+++ src/components/News.js
@@ -60,7 +60,7 @@ const News = (props) => {
         <div className='container my-4'>
             <h1 className="mb-3 text-center" style={{marginTop:'70px'}} >NewsMonkey - Top {capitalizeFirstLetter(props.category)} Headlines</h1>
             {loading && <SpinnerLoad />}
-            {article && <InfiniteScroll dataLength={article.length} next={fetchMoreData} hasMore={article.length !== totalResults} loader={<SpinnerLoad />}>
+            <InfiniteScroll dataLength={article.length} next={fetchMoreData} hasMore={article.length !== totalResults} loader={<SpinnerLoad />}>
                 <div className="container">
                     <div className="row">
                         {article.map((element) => {
@@ -70,7 +70,7 @@ const News = (props) => {
                         })}
                     </div>
                 </div>
-            </InfiniteScroll>}
+            </InfiniteScroll>
         </div>
     )
 }
